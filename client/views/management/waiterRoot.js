dfeb08ff4cf6e6f704d669bb03762862e83501ba
Ed <odesey@gmail.com>
2014-03-18 12:45:45 -0400

    added code to limit code field to numeric values only, also we now check that the table code exists in the Tables collection

---
+++ client/views/management/waiterRoot.js
@@ -0,0 +1,18 @@
+Template.waiterRoot.events({
+	'click #addTable': function (e) {
+		e.preventDefault();
+		var tableNumber = Math.floor(Math.random()*90000) + 10000;
+		var table = {
+			number: tableNumber, 
+			creator: $('#name').val()
+		}
+		table._id = Tables.insert(table);
+		console.log('add a new table with ID of ' + tableNumber);
+	}
+});
+
+Template.waiterRoot.helpers({
+	myTables: function () {
+		return Tables.find();
+	}
+});
--- client/views/redirect/redirect.js
+++ client/views/redirect/redirect.js
@@ -1,16 +1,32 @@
 Template.reDirect.events({
   'submit form': function(e) {
     e.preventDefault();
-    var code = $("#tableCode").val()
-    if (code.length == 5) {
-      console.log('redirect to the menu page');
-      Router.go('menuRoot');
-    } else if (code.length == 7) {
-      Router.go('waiterRoot');
-      console.log('redirect to the Waiter Manager page');
-    } else {
-      console.log('you entered a wrong code')
+    var code = $("input#tableCode").val();
+    // check if the code field is not empty and is only numeric characters
+    if (code !== "" && $.isNumeric(code)) {
+      if (code.length == 5) {
+        console.log('redirect to menu root page');
+        // var codeCheck = Tables.find({number: code}).fetch().length;
+        if (Tables.find({number: Number(code)}).count() !== 0) {
+          Router.go('menuRoot');
+        } else {
+          console.log('code not found in table DB');
+        };
+        
+      } else if (code.length == 7) {
+        console.log('redirect to waiter root page');
+        Router.go('waiterRoot');
+      } else {
+        console.log('invalid code');
+      };
+      
     };
+    
+  }
+});
 
+Template.waiterRoot.helpers({
+  tables: function () {
+    return Tables.find();
   }
 });
