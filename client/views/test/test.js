8a33b000bf5fc73b25b367309c444f16dd9c207c
Ed <odesey@gmail.com>
2014-04-25 15:03:38 -0400

    clearly i should have paid more attention in math class, after 5 days I solved the individual order subdivision problem.....my former math teacher are laughing right about now

--- client/views/test/test.js
+++ client/views/test/test.js
@@ -1,3 +1,5 @@
+var someVar = "test";
+
 Template.test.helpers({
   tableBallance: function () {
   	var ballance = Tabs.findOne({table: Session.get('tableID')});
@@ -41,7 +43,7 @@ Template.test.events({
     e.preventDefault()
     $(e.target).parent('tr').nextUntil('.orders').slideToggle(100);
   },
-  'change input[type=checkbox].checkbox': function (e) {
+'change input[type=checkbox].checkbox': function (e) {
     var currentTR = $(e.target).parents().eq(1).prevAll("tr.orders:first");
     var checkedTotal = $(currentTR).nextUntil('.orders').find(".checkbox");
     var splitTotal = 0;
@@ -49,6 +51,9 @@ Template.test.events({
     var checked = !!e.target.checked;
     var guests = $(".quick-button-small");
     var checkBoxes = $(event.target).parents().eq(1).prevAll("tr.orders:first").nextUntil('.orders').find(".checkbox");
+    var guestName = $(e.target).parent().text();
+    var clickGuestPriorTotal = $(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1);
+    var itemPrice = Number($(e.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text()).toFixed(2);
 
     checkedTotal.each(function(index, el) {
       if (el.checked) {
@@ -65,35 +70,89 @@ Template.test.events({
 
     if (checked) {
       // only if this is the first person being assigned to a ordered item 
-      var guestName = $(e.target).parent().text();
-      var itemPrice = $(e.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text();
-        if (splitTotal == 1) {
-          $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+Number(itemPrice));
-        } else {
+      // var guestName = $(e.target).parent().text();
+      // var itemPrice = $(e.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text();
+        // if (splitTotal == 1) {
+        //   var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
+        //   $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal + Number(itemPrice)).toFixed(2));
+        // } else {
           // if we are adding more than one person to an ordered item we need to adjust the percentages
           // first we increase the price of the person being added to the order by the correct percent
-          console.log('when will this run?');
-          itemPrice = (itemPrice / splitTotal).toFixed(2);
-          var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
-          $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal + Number(itemPrice)).toFixed(2));
+          // console.log('when will this run?');
+          // var itemPrice = (itemPrice / splitTotal).toFixed(2);
+          // var _itemPrice = itemPrice;
+          // var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
+          // $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal + Number(itemPrice)).toFixed(2));
           // then we decrease the price for the folks already sharing the order
+          checkBoxes.each(function(index, el) {
+            if (el.checked) {
+              var guestName = $(el).parent().text();
+              // var clickedGuest = $(event.target).parent().text();
+              // var clickGuestPriorTotal = $(".quick-button-small").find('.name:contains('+clickedGuest+')').next().text().substring(1);
+              // var itemPrice = $(event.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text();
+              var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
+              var adjustPercent = (itemPrice/splitTotal)/itemPrice*100;
+              var adjustmentNumber = (itemPrice - ((adjustPercent / 100) * itemPrice));
+
+              // console.log(testPercent+" "+"this is how much i should be subtracting from my existingTotal"); 
+              console.log(adjustPercent+" "+"percentage i need to adjust the price by for existing guests sharing this order");
+              console.log(itemPrice+" "+"this number minus previous number % ");
+              console.log(existingTotal+" "+"existingTotal number I will be subtracting from");
+              console.log(adjustmentNumber+" "+"this is the number i am subtracting from the existing Total");
+              console.log(itemPrice/splitTotal+" "+ "simple division of itemPrice")
+              console.log(existingTotal - ((adjustPercent / 100) * itemPrice));
+              console.log((itemPrice/100).toFixed(2));
+              console.log(clickGuestPriorTotal+" "+"prior total for clied guest");
+              
+              if ($(el).parent().text() == $(event.target).parent().text()) {
+                $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal + (itemPrice/splitTotal)).toFixed(2));
+              } else {
+                if (Number(clickGuestPriorTotal) !== 0) {
+                  // $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - (itemPrice/splitTotal)).toFixed(2));
+                  $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+((existingTotal - (itemPrice/(splitTotal-1))) + (itemPrice/splitTotal)).toFixed(2));
+                } else {
+                  $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal * (1 - (itemPrice/splitTotal)/itemPrice)).toFixed(2));
+                };
+              };
+
+              // if ((Number(existingTotal)*(adjustPercent/100)).toFixed(2) !== adjustmentNumber.toFixed(2)) {
+              //   var adjustmentNumber = (itemPrice - ((adjustPercent / 100) * itemPrice));
+              //   $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - adjustmentNumber).toFixed(2));
+              // } else {
+                // $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - adjustmentNumber).toFixed(2));
+              // };
+              // itemPrice = ((itemPrice/splitTotal)/itemPrice*100)/100*existingTotal;
+              // itemPrice = ((itemPrice/splitTotal)/itemPrice*100)/100*itemPrice
+              // var itemPrice = Number(existingTotal) > 0 && (guestName !== $(event.target).parent().text()) ? ((itemPrice/splitTotal)/itemPrice*100)/100*existingTotal : ((itemPrice/splitTotal)/itemPrice*100)/100*itemPrice;
+              // $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - adjustmentNumber).toFixed(2));
+            };
+          })
+        } else {
+        // var guestName = $(e.target).parent().text();
+        // var itemPrice = $(e.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text();
+        // only if this is the last person being removed from an ordered item
+        if (splitTotal == 0) {
+          var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
+          $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - Number(itemPrice)).toFixed(2));
+        } else {
+          console.log('update the boxes that are still checked.');
+          itemPrice = (itemPrice / (splitTotal+1)).toFixed(2);
+          var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
+          $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - Number(itemPrice)).toFixed(2));
+          // then we increase the price for the folks still sharing the order
           checkBoxes.each(function(index, el) {
             if (el.checked && ($(el).parent().text() !== $(event.target).parent().text())) {
               var guestName = $(el).parent().text();
               var itemPrice = $(event.target).parents().eq(1).prevAll("tr.orders:first").children().eq(1).text();
               var existingTotal = Number($(".quick-button-small").find('.name:contains('+guestName+')').next().text().substring(1));
-              itemPrice = ((itemPrice/splitTotal) / itemPrice * 100) / 100 * existingTotal;
+              var adjustPercent = (itemPrice/splitTotal)/itemPrice*100;
+              var adjustmentNumber = (existingTotal + ((adjustPercent / 100) * itemPrice));
+              // itemPrice = ((itemPrice/splitTotal) / itemPrice * 100) / 100 * existingTotal;
+              // var itemPrice = Number(existingTotal) > 0 ? ((itemPrice/splitTotal)/itemPrice*100)/100*existingTotal : ((itemPrice/splitTotal)/itemPrice*100)/100*itemPrice;
 
-              $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal - itemPrice).toFixed(2));
+              $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$"+(existingTotal + adjustmentNumber).toFixed(2));
             };
           })
-        };
-
-      } else {
-        // only if this is the last person being removed from an ordered item
-        if (splitTotal == 0) {
-          var guestName = $(e.target).parent().text();
-          $(".quick-button-small").find('.name:contains('+guestName+')').next().text("$0");
         }
       };
 
